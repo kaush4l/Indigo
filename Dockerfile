38530ae7b2e2d3d156fe67fd38271bc979f3fd20
Kaushal <kaushal.beyblader@gmail.com>
2020-08-09 23:47:33 -0700

    Changes to use gradle/maven dynamic scripts

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 # docker different base images for running using 
-# (1) java 105mb
+# java 105mb
 FROM openjdk:8-jdk-alpine
 
+# Below images not needed as both Maven/Gradle provide dynamic run scripts
 # (2) maven 220mb
 # FROM maven:3.6.3-jdk-8-openj9 
-
 # (3) gralde 230mb
 # FROM gradle:jre8
 
@@ -15,22 +15,27 @@ EXPOSE 8080
 
 # # Running them using
 # # (1) Java (build jar outside)
+# Since already built, can use jar file to run directly
 ARG JAR_FILE=target/Indigo-0.0.1.jar
 ADD ${JAR_FILE} Indigo-0.0.1.jar
 ENTRYPOINT ["java","-jar","Indigo-0.0.1.jar"]
 
+# Build application using
+
 # (2) (3) for building inside docker using maven/gradle
 # COPY /src/main /src/main
 
-# Build application using
-
 # (2) using maven
 # COPY pom.xml pom.xml
-# RUN mvn clean install
-# CMD ["mvn", "spring-boot:run"]
+# COPY .mvn .mvn
+# COPY mvnw mvnw
+# RUN ./mvnw clean install
+# CMD ["./mvnw", "spring-boot:run"]
 
 # (3) using gradle
 # COPY build.gradle build.gradle
 # COPY settings.gradle settings.gradle
-# RUN gradle clean build
-# CMD ["gradle", "bootRun"]
+# COPY gradle gradle
+# COPY gradlew gradlew
+# RUN ./gradlew build
+# CMD ["./gradlew", "bootRun"]
--- buildEnv.sh
+++ buildEnv.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# $(git clone https://github.com/kaush4l/Indigo/)
+# $(git clone https://github.com/kaush4l/Indigo_client/)
+
 # Enforcing pre commit hooks to run tests before pushing
 echo "Copying files to enforce git hooks."
 cp hooks/pre-commit .git/hooks/pre-commit
@@ -7,49 +10,20 @@ cp hooks/pre-commit .git/hooks/pre-commit
 # Checking for Java installation
 echo "----------------------------------------JAVA----------------------------------------"
 JAVA=$(java -version)
-if [ $? -eq 0 ]
-then
+if [ $? -eq 0 ] then
     echo "Java is present. make sure version is 1.8."
 else
     echo "Java has not been installed yet."
 fi
 echo "----------------------------------------JAVA----------------------------------------"
 
-# Check if maven is installed
-echo "----------------------------------------MAVEN----------------------------------------"
-MAVEN=$(mvn -v)
-if [ $? -eq 0 ]
-then
-    echo "Maven is installed."
-    MAVENBUILD=$(gradle clean build)
-    if [ $? -eq 0 ]
-    then
-        echo "Maven build is done and code is ready to use."
-    else
-        echo "Run mvn clean install for the project."
-    fi
+# BUILD = $(.mvnw clean install) # Maven build
+BUILD=$(./gradlew clean build) # Gradle build
+if [ $? -eq 0 ] then
+    echo "build is done and subsequent builds will be faster with gradlew."
 else
-    echo "Maven is not installed. Either maven or gradle is good for learning of the project."
+    echo "Can use either ./gradlew or ./mvnw files for the builds."
 fi
-echo "----------------------------------------MAVEN----------------------------------------"
-
-# Check if gradle is installed
-echo "----------------------------------------GRADLE----------------------------------------"
-GRADLE=$(gradle -v)
-if [ $? -eq 0 ]
-then
-    GRADLEBUILD=$(gradle clean build)
-    echo "Gradle is installed."
-    if [ $? -eq 0 ]
-    then
-        echo "Gradle build is done and subsequent builds will be faster."
-    else
-        echo "Run gradle clean build for the project."
-    fi
-else
-    echo "Maven is not installed. Either maven or gradle is good for learning of the project."
-fi
-echo "----------------------------------------GRADLE----------------------------------------"
 
 # Check if docker is installed
 echo "----------------------------------------DOCKER----------------------------------------"
@@ -58,6 +32,6 @@ if [ $? -eq 0 ]
 then
     echo "Docker is installed."
 else
-    echo "Project is Dockerized. Good to have not necessary."
+    echo "Project is Dockerized. So, good to have but not necessary."
 fi
 echo "----------------------------------------DOCKER----------------------------------------"
